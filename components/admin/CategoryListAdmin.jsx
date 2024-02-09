03e0eb9574aab535905fa9c1534146117e2208dc
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2024-02-09 17:40:05 +0700

    feat(admin) create screen without functionalities

---
+++ components/admin/CategoryListAdmin.jsx
@@ -0,0 +1,71 @@
+import React, { useCallback } from 'react';
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import {COLORS, SIZES} from "../../constants";
+import { AddIcon, Icon, RemoveIcon } from "@gluestack-ui/themed";
+import { useOrder } from "../../hooks/Order";
+import { formatCurrency } from "../../utils/formatCurrency";
+import debounce from 'lodash/debounce';
+
+const ProductListAdmin = ({ item }) => {
+    return (
+        <TouchableOpacity style={{
+            flexDirection: "row", gap: SIZES.small, justifyContent: "space-between", marginTop: SIZES.small,
+            borderBottomWidth: .5,
+            borderColor: COLORS.gray,
+            padding: SIZES.xxSmall
+        }}>
+            <View style={{
+                flexDirection: "row", gap: SIZES.small
+            }}>
+                <Image
+                    source={{uri: item.image_url}}
+                    resizeMode={'cover'}
+                    style={{
+                        width: 60,
+                        height: 60,
+                        borderRadius: SIZES.small,
+                    }}/>
+
+                <View style={{
+                    gap: SIZES.light,
+                    paddingTop: SIZES.light
+                }}>
+                    <Text numberOfLines={1} ellipsizeMode="tail" style={{
+                        fontSize: SIZES.medium,
+                        fontWeight: '600',
+                    }}>{item?.name}</Text>
+
+                    <Text numberOfLines={1} ellipsizeMode="tail" style={{
+                        color: COLORS.darkGray
+                    }}>Price : {formatCurrency(2000)}</Text>
+                </View>
+            </View>
+
+            <View style={{
+                alignItems: "flex-end",
+                justifyContent: 'center',
+                flexDirection: "row",
+                gap: SIZES.medium,
+                paddingVertical: SIZES.xxSmall
+            }}>
+
+                <Text numberOfLines={1} ellipsizeMode="tail" style={{
+                    color: COLORS.darkGray
+                }}>x100</Text>
+                {/*<TouchableOpacity onPress={debouncedHandleDecreaseProduct}>*/}
+                {/*    <Icon as={RemoveIcon} w="$5" h="$5" />*/}
+                {/*</TouchableOpacity>*/}
+
+                {/*<Text style={{*/}
+                {/*    fontSize: SIZES.medium*/}
+                {/*}}>{item?.qty}</Text>*/}
+
+                {/*<TouchableOpacity onPress={debouncedHandleAddProduct}>*/}
+                {/*    <Icon as={AddIcon} w="$5" h="$5" />*/}
+                {/*</TouchableOpacity>*/}
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+export default ProductListAdmin;
---
+++ components/common/TableCustomNotUsed.jsx
@@ -0,0 +1,48 @@
+import {Text, TouchableOpacity} from "react-native";
+import {COLORS, SIZES} from "../../constants";
+import React from "react";
+import {Badge, BadgeText} from "@gluestack-ui/themed";
+
+const TableCustom = ({
+                         children, handlePress = () => {
+    }, item, isSelected, isTakeaway
+                     }) => (
+    <TouchableOpacity
+        onPress={() => handlePress(item)}
+        style={{
+            width: '100%', // Set a fixed width for each item
+            alignItems: 'center',
+            minHeight: 140,
+            justifyContent: 'center',
+            borderRadius: SIZES.small,
+            borderWidth: .5,
+            borderColor: COLORS.lightWhite,
+            marginRight: SIZES.small, // Add margin between items
+            backgroundColor: isSelected ? COLORS.primary : COLORS.white,
+            shadowColor: "rgba(0,0,0,0.28)",
+            shadowOffset: {
+                width: 2,
+                height: 2,
+            },
+            shadowOpacity: 0.10,
+            shadowRadius: 3.84,
+            elevation: 2,
+
+        }}
+    >
+        <Badge size="md" style={{
+            position: 'absolute',
+            zIndex: 2,
+            right: SIZES.small,
+            top: SIZES.xSmall
+        }} variant="solid" borderRadius="$none" action={isTakeaway ? "warning" : "success"}>
+            <BadgeText>{item?.is_takeaway}</BadgeText>
+        </Badge>
+        <Text style={{
+            color: isSelected ? 'white' : 'black',
+            fontWeight: '600',
+        }}>{children}</Text>
+    </TouchableOpacity>
+);
+
+export default TableCustom;
